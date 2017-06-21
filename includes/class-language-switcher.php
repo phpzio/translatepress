6ef6491bb594969997446e4e2edf1d1f4bc05e7b
Cristophor <cristophor22@gmail.com>
2017-06-21 16:55:55 +0300

    Work on floater language switcher

--- includes/class-language-switcher.php
+++ includes/class-language-switcher.php
@@ -11,6 +11,9 @@ class TRP_Language_Switcher{
         $language = $this->get_current_language();
         global $TRP_LANGUAGE;
         $TRP_LANGUAGE = $language;
+
+        // TODO: Floater
+        add_action( 'wp_footer', array( $this, 'add_floater_language_switcher' ) );
     }
 
     public function language_switcher(){
@@ -58,6 +61,9 @@ class TRP_Language_Switcher{
 
     public function enqueue_language_switcher_scripts( ){
         wp_enqueue_script('trp-dynamic-translator', TRP_PLUGIN_URL . 'assets/js/trp-language-switcher.js', array('jquery'));
+        // TODO: Floater
+        wp_enqueue_script( 'trp-floater-language-switcher-script', TRP_PLUGIN_URL . 'assets/js/trp-floater-language-switcher.js', array( 'jquery' ) );
+        wp_enqueue_style( 'trp-floater-language-switcher-style', TRP_PLUGIN_URL . 'assets/css/trp-floater-language-switcher.css' );
     }
 
     public function add_language_to_home_url( $url, $path, $orig_scheme, $blog_id ){
@@ -73,4 +79,46 @@ class TRP_Language_Switcher{
 
         return apply_filters( 'trp_home_url', $new_url, $abs_home, $TRP_LANGUAGE, $path );
     }
+
+    // TODO: Floater
+    public function add_floater_language_switcher() {
+
+        global $TRP_LANGUAGE;
+
+        $published_languages = TRP_Utils::get_language_names( $this->settings['publish-languages'] );
+
+        $trp_floater_ls_names = ( isset( $this->settings['trp-ls-floater-names'] ) && $this->settings['trp-ls-floater-names'] == 'yes' ? 'name' : 'code' );
+        $trp_floater_ls_class = ( $trp_floater_ls_names == 'name' ? 'trp-floater-ls-name' : 'trp-floater-ls-code' );
+
+        $current_language = '';
+        $other_languages = array();
+        foreach( $published_languages as $code => $name ) {
+            if( $code == $TRP_LANGUAGE ) {
+                $current_language = ( $trp_floater_ls_names == 'name' ? ucfirst( $name ) : strtoupper( $code ) );
+            } else {
+                $other_languages[$code] = $name;
+            }
+        }
+
+        ?>
+        <div id="trp-floater-ls" class="<?php echo $trp_floater_ls_class ?>">
+            <div id="trp-floater-ls-current-language">
+                <a href="javascript:void(0)" class="trp-floater-ls-disabled-language" onclick="void(0)"><?php echo $current_language ?></a>
+            </div>
+            <div id="trp-floater-ls-language-list">
+                <?php
+                foreach( $other_languages as $code => $name ) {
+                    $language_label = ( $trp_floater_ls_names == 'name' ? $name : strtoupper( $code ) )
+                    ?>
+                    <a href="javascript:void(0)" title="<?php echo $name ?>" onclick="trp_floater_change_language( '<?php echo $code ?>' )"><?php echo $language_label ?></a>
+                <?php
+                }
+                ?>
+                <a href="javascript:void(0)" class="trp-floater-ls-disabled-language"><?php echo $current_language ?></a>
+            </div>
+        </div>
+
+    <?php
+    }
+
 }
