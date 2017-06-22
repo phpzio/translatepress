d72a0e8a0d821ea04fb4bfaac0720dcdd7f9104d
Razvan Mocanu <razvan@cozmoslabs.com>
2017-06-22 15:20:18 +0300

    Registered post_type as menu item. Replaced permalink of menu item with language version of current page.

--- includes/class-language-switcher.php
+++ includes/class-language-switcher.php
@@ -73,19 +73,70 @@ class TRP_Language_Switcher{
         return apply_filters( 'trp_home_url', $new_url, $abs_home, $TRP_LANGUAGE, $path );
     }
 
-    public function add_ls_to_menu( $items, $args ){
-        if ( isset ( $this->settings['trp-ls-menu'] ) && $this->settings['trp-ls-menu'] == 'yes' && !empty ( $this->settings['trp-ls-menu-select'] ) ) {
-            // todo this
-            if ( isset( $args->menu->term_id ) && $args->menu->term_id == $this->settings['trp-ls-menu-select'] ) {
-                ob_start();
-                global $TRP_LANGUAGE;
-                $current_language = $TRP_LANGUAGE;
-                $published_languages = TRP_Utils::get_language_names($this->settings['publish-languages']);
-                require TRP_PLUGIN_DIR . 'includes/partials/language-switcher-menu.php';
-                $ls_html = ob_get_clean();
-                $items .= $ls_html;
+    public function add_ls_to_menu( ){
+        $args = array(
+            'exclude_from_search'   => true,
+            'publicly_queryable'    => false,
+            'show_ui'               => true,
+            'show_in_nav_menus'     => true,
+            'show_in_menu'          => false,
+            'show_in_admin_bar'     => false,
+            'can_export'            => false,
+            'public'                => false,
+            'label'                 => 'Language Switcher'
+        );
+        register_post_type( 'language-switcher', $args );
+
+
+
+        //todo  move this to settings, add check if exists before insert post
+        $published_languages = TRP_Utils::get_language_names( $this->settings['publish-languages'] );
+
+        foreach ( $published_languages as $language_code => $language_name ) {
+
+            $ls = array(
+                'post_title'    => $language_name,
+                'post_content'  => $language_code,
+                'post_status'   => 'publish',
+                'post_type'     => 'language-switcher'
+            );
+            //error_log(json_encode(wp_insert_post( $ls )));
+        }
+    }
+
+    public function ls_menu_permalinks( $items, $menu, $args ){
+
+        //error_log( serialize( $items ) );
+        foreach ( $items as $key => $item ){
+            if ( $item->object == 'language-switcher' ){
+                $ls_id = get_post_meta( $item->ID, '_menu_item_object_id', true );
+                $ls_post = get_post( $ls_id );
+                $items[$key]->url = $this->url_converter->get_url_for_language( $ls_post->post_content );
             }
         }
+
         return $items;
+
+
+
+       /* $args = array(
+            'menu-item-url' => 'URRRRLLL',
+             'menu-item-title' => 'DIFFFERENT',
+        );
+        //wp_update_nav_menu_item(7,480,$args);
+        /*ls_menu_permalinks
+        if ( $post->post_type !== 'language-switcher' ){
+            /*
+            //error_log('it\'s LS');
+            //return 'http://www.googgle.com/';
+            return $permalink;
+        }
+
+        error_log('ls');
+        //error_log( $post->ID );
+        return $permalink . "/something-new/";*/
+
     }
+
+
 }
