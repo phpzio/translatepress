d96097a77efa2faec3a00011accdd0f3c9b337ac
Cristophor Hurduban <cristophor22@gmail.com>
2017-07-17 23:15:50 +0300

    Working on language switcher shortcode select

--- assets/js/trp-language-switcher.js
+++ assets/js/trp-language-switcher.js
@@ -6,25 +6,27 @@ function trp_change_language( select ){
 }
 
 jQuery( document ).ready( function( ) {
-    jQuery.widget( "custom.iconselectmenu", jQuery.ui.selectmenu, {
+    jQuery.widget( 'trp.iconselectmenu', jQuery.ui.selectmenu, {
         _renderItem: function( ul, item ) {
-            var li = jQuery( "<li class='trp-ls-li'>" ),
-                wrapper = jQuery( "<div style='display: inline-block;'>" );
+            var li = jQuery( '<li class="trp-ls-li">' );
+            var wrapper = jQuery( '<div style="display: inline-block;">' );
 
             if( item.disabled ) {
-                li.addClass( "ui-state-disabled" );
+                li.addClass( 'ui-state-disabled' );
             }
 
-            jQuery( "<span>", { text: jQuery.trim( item.label ) } ).appendTo( wrapper );
+            jQuery( '<span>', {
+                text: jQuery.trim( item.label )
+            } ).appendTo( wrapper );
 
-            jQuery( "<span>", {
-                style: item.element.attr( "data-style" ),
-                "class": "ui-icon " + item.element.attr( "data-class" )
+            jQuery( '<span>', {
+                style: item.element.attr( 'data-style' ),
+                'class': 'ui-icon ' + item.element.attr( 'data-class' )
             } ).prependTo( wrapper );
 
             return li.append( wrapper ).appendTo( ul );
         }
-    });
+    } );
 
     jQuery( '.trp-language-switcher-select' ).each( function() {
         jQuery( this )
@@ -36,16 +38,14 @@ jQuery( document ).ready( function( ) {
                 select: function( event, ui ) {
                     jQuery( this ).closest( 'form.trp-language-switcher-form' ).find( '.trp-current-language-icon' )
                         .css( 'background-image', 'url(' + jQuery( this ).find( 'option[value="' + ui.item.value + '"]' ).data( 'flag-url' ) + ')' );
-                    if( typeof parent.trpEditor == 'undefined' ) {
+                    /*if( typeof parent.trpEditor == 'undefined' ) {
                         window.location.replace( document.querySelector( 'link[hreflang="' + ui.item.value + '"]' ).href );
-                    }
+                    }*/
                 },
                 icons: {
-                    button: "trp-current-language-icon"
+                    button: 'trp-current-language-icon'
                 }
-            } ).iconselectmenu( "menuWidget" ).addClass( "ui-menu-icons trp-avatar" );
-
-
+            } ).iconselectmenu( 'menuWidget' ).addClass( 'ui-menu-icons trp-flag-icon' );
     } );
 
     /*jQuery( ".trp-language-switcher-select" )
@@ -61,11 +61,11 @@ jQuery( document ).ready( function( ) {
         .addClass( "ui-menu-icons trp-avatar" );*/
 
     jQuery( '<span>', {
-        "class": "dashicons dashicons-arrow-down"
+        'class': 'dashicons dashicons-arrow-down'
     } ).appendTo( jQuery( 'form.trp-language-switcher-form .ui-selectmenu-button' ) );
 
     jQuery( '.ui-state-default .ui-icon.trp-current-language-icon' ).each( function() {
-        jQuery( this ).css( 'background-image', 'url( http://local.wordpress.dev/wp-content/plugins/translate-press/assets/images/flags/en_US.png )' );
+        jQuery( this ).css( 'background-image', 'url(' + jQuery( this ).closest( 'form.trp-language-switcher-form' ).find( 'select.trp-language-switcher-select' ).find( ':selected' ).data( 'flag-url' ) + ')' );
     } );
 
 } );
