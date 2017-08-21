e3896e86c52de4c4b96f15937e880a490cdff4d5
MadalinUngureanu <ungureanu.madalin@gmail.com>
2017-08-21 15:47:28 +0300

    we can select gettext texts in buttons now too

--- assets/js/trp-editor-script.js
+++ assets/js/trp-editor-script.js
@@ -1036,7 +1036,14 @@ jQuery(function(){
                     trpEditor.edit_translation_button = jQuery( '<span class="trp-edit-translation"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13.89 3.39l2.71 2.72c.46.46.42 1.24.03 1.64l-8.01 8.02-5.56 1.16 1.16-5.58s7.6-7.63 7.99-8.03c.39-.39 1.22-.39 1.68.07zm-2.73 2.79l-5.59 5.61 1.11 1.11 5.54-5.65zm-2.97 8.23l5.58-5.6-1.07-1.08-5.59 5.6z"></path></svg></span>' );
                 }
 
-                jQuery(this).prepend( trpEditor.edit_translation_button );
+                if ( jQuery(this).attr( 'type' ) == 'submit' || jQuery(this).attr( 'type' ) == 'button' || jQuery(this).attr('type') == 'search' ) {
+                    if( jQuery(this).parent('trp-wrap').length == 0 )
+                        jQuery(this).wrap('<trp-wrap class="trpgettext-wrap"></trp-wrap>');
+                    jQuery(this).parent().prepend(trpEditor.edit_translation_button);
+                }
+                else {
+                    jQuery(this).prepend(trpEditor.edit_translation_button);
+                }
                 trpEditor.edit_translation_button.off( 'click' );
                 trpEditor.edit_translation_button.on( 'click', function(e){
                     e.preventDefault();
