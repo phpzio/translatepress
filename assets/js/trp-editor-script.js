48c58760f5d87a674495f3bffa884935021ba820
MadalinUngureanu <ungureanu.madalin@gmail.com>
2018-03-14 17:39:46 +0200

    fixed issue with normal text nodes that were inside an element that had an atribute with gettext and did not get translated

--- assets/js/trp-editor-script.js
+++ assets/js/trp-editor-script.js
@@ -799,7 +799,8 @@ function TRP_String( language, array_index ){
     /**
      * Show the pencil and border the viewable string in Preview window.
      */
-    this.highlight = function (){
+    this.highlight = function (e){
+        e.stopPropagation();
         if ( ! trpEditor.edit_translation_button ){
             _this.jquery_object.prepend( '<span class="trp-edit-translation"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13.89 3.39l2.71 2.72c.46.46.42 1.24.03 1.64l-8.01 8.02-5.56 1.16 1.16-5.58s7.6-7.63 7.99-8.03c.39-.39 1.22-.39 1.68.07zm-2.73 2.79l-5.59 5.61 1.11 1.11 5.54-5.65zm-2.97 8.23l5.58-5.6-1.07-1.08-5.59 5.6z"></path></svg></span>' );
             trpEditor.edit_translation_button = _this.jquery_object.children('.trp-edit-translation');
