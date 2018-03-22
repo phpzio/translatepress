a3ca503fa506cc6802a9a982a6ed120226b0fff7
Cristian Antohe <sareiodata@gmail.com>
2018-03-22 14:26:22 +0200

    fix widget language switcher to take into account the new hreflang

--- partials/language-switcher-shortcode.php
+++ partials/language-switcher-shortcode.php
@@ -9,7 +9,7 @@ if( $shortcode_settings['flags'] && ! $shortcode_settings['full_names'] && ! $sh
     <form class="trp-language-switcher-form <?php echo $only_flags_class ?>" action="" method="POST">
         <select data-no-translation class="trp-language-switcher-select" <?php echo ( isset( $_GET['trp-edit-translation'] ) && $_GET['trp-edit-translation'] == 'preview' ) ? 'data-trp-unpreviewable="trp-unpreviewable"' : '' ?> name="lang" onchange='trp_change_language( this )'>
             <?php foreach ( $published_languages as $code => $name ){ ?>
-            <option title="<?php echo ucfirst( $name ) ?>" data-class="trp-flag-icon" data-style="background-image: url( <?php echo $this->add_flag( $code, $name, 'ls_shortcode' ) ?> );" data-flag-url="<?php echo $this->add_flag( $code, $name, 'ls_shortcode' ) ?>" value="<?php echo $code ?>" <?php echo ( $current_language == $code ) ? 'selected' : '' ?> >
+            <option title="<?php echo ucfirst( $name ) ?>" data-class="trp-flag-icon" data-style="background-image: url( <?php echo $this->add_flag( $code, $name, 'ls_shortcode' ) ?> );" data-flag-url="<?php echo $this->add_flag( $code, $name, 'ls_shortcode' ) ?>" value="<?php echo str_replace("_", "-", $code );?>" <?php echo ( $current_language == $code ) ? 'selected' : '' ?> >
                 <?php
 
                 if( $shortcode_settings['full_names'] ) {
