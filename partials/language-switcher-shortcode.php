0bd031037db6b56b2d28d01ce1aaeb78e878da1c
Cristian Antohe <sareiodata@gmail.com>
2018-06-04 15:26:55 +0300

    added 5px to the language switcher shortcode so it doesn't brake on iOS

--- partials/language-switcher-shortcode.php
+++ partials/language-switcher-shortcode.php
@@ -7,7 +7,7 @@ $current_language_preference = $this->add_shortcode_preferences($shortcode_setti
         jQuery( document ).ready(function(){
             // need to have the same with set from JS on both divs. Otherwise it can push stuff around in HTML
             jQuery('.trp-language-switcher').each(function(index, el){
-                var trp_ls_shortcode_width = jQuery(el).find('.trp-ls-shortcode-language').width();
+                var trp_ls_shortcode_width = jQuery(el).find('.trp-ls-shortcode-language').width() + 5;
                 jQuery(el).find('.trp-ls-shortcode-language').width(trp_ls_shortcode_width);
                 jQuery(el).find('.trp-ls-shortcode-current-language').width(trp_ls_shortcode_width);
                 // We're putting this on display: none after we have it's width.
