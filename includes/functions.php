6438c84e9f29cb918bfdb3b6f8baf39df36deb96
Razvan Mocanu <razvan@cozmoslabs.com>
2018-12-05 18:10:36 +0200

    Strip trpst tags from urls run through sanitize_title and esc_url

--- includes/functions.php
+++ includes/functions.php
@@ -304,28 +304,6 @@ function trp_use_home_url_in_peepso_ajax( $data ){
     return $data;
 }
 
-/**
- * Filter sanitize_title() to use our own remove_accents() function so it's based on the default language, not current locale.
- *
- * @since 1.3.1
- *
- * @param string $title
- * @param string $raw_title
- * @param string $context
- * @return string
- */
-add_filter( 'sanitize_title', 'trp_sanitize_title', 1, 3 );
-function trp_sanitize_title(  $title, $raw_title, $context ){
-
-    if ( 'save' == $context )
-        $title = trp_remove_accents( $raw_title );
-
-    remove_filter( 'sanitize_title', 'trp_sanitize_title', 1, 3 );
-    $title = apply_filters( 'sanitize_title', $title, $raw_title, $context );
-    add_filter( 'sanitize_title', 'trp_sanitize_title', 1, 3 );
-
-    return $title;
-}
 
 function trp_remove_accents( $string ){
 
