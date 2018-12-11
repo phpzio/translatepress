ec3bcda97856c7cf67564db59324d10c7ed17062
MadalinUngureanu <ungureanu.madalin@gmail.com>
2018-12-11 10:40:45 +0200

    added testing infrastructure

---
+++ tests/test-translatepress.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Translatepress
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+        $this->assertEquals( 'http://www.url.com' , 'hjjhjhjh');
+	}
+
+    public function test_samples() {
+        // Replace this with some actual testing code.
+        $this->assertEquals( 'http://www.url.com' , 'hjjhjhjh');
+    }
+}
