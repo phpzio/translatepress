009df22a58a06030d8faf66188d8319e9edf56d6
MadalinUngureanu <madalin@reflectionmedia.ro>
2018-12-19 14:41:13 +0200

    started writing speciffic tests

--- tests/test-translatepress.php
+++ tests/test-translatepress.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Class SampleTest
+ * Class TranslatePressTest
  *
  * @package Translatepress
  */
@@ -8,18 +8,19 @@
 /**
  * Sample test case.
  */
-class SampleTest extends WP_UnitTestCase {
+class TranslatePressTest extends WP_UnitTestCase {
 
 	/**
 	 * A single example test.
 	 */
 	public function test_sample() {
 		// Replace this with some actual testing code.
-        $this->assertEquals( 'http://www.url.com' , 'hjjhjhjh');
-	}
+        //$this->assertEquals( 'http://www.url.com' , 'hjjhjhjh');
 
-    public function test_samples() {
-        // Replace this with some actual testing code.
-        $this->assertEquals( 'http://www.url.com' , 'hjjhjhjh');
-    }
+        $trp_settings = new TRP_Settings();
+        $trp_render = new TRP_Translation_Render( $trp_settings->get_settings() );
+
+        $this->assertEquals( $trp_render->translate_page( 'this is a text' ), 'this is a text' ) ;
+
+	}
 }
