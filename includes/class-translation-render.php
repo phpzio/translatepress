cefed2d1b3f8c12004cce13b3be0f5eaf00a7644
Razvan Mocanu <razvan@cozmoslabs.com>
2019-04-02 16:08:24 +0300

    Check if str_get_html is succesful to avoid fatal error

--- includes/class-translation-render.php
+++ includes/class-translation-render.php
@@ -361,6 +361,11 @@ class TRP_Translation_Render{
 		    $merge_rules = $this->translation_manager->get_merge_rules();
 	    }
         $html = TranslatePress\str_get_html($output, true, true, TRP_DEFAULT_TARGET_CHARSET, false, TRP_DEFAULT_BR_TEXT, TRP_DEFAULT_SPAN_TEXT);
+	    if ( $html === false ){
+		    $trpremoved = preg_replace( '/(<|&lt;)trp-gettext (.*?)(>|&gt;)/', '', $output );
+		    $trpremoved = preg_replace( '/(<|&lt;)(\\\\)*\/trp-gettext(>|&gt;)/', '', $trpremoved );
+			return $trpremoved;
+	    }
 
         /**
          * When we are in the translation editor: Intercept the trp-gettext that was wrapped around all the gettext texts, grab the attribute data-trpgettextoriginal
@@ -450,6 +455,9 @@ class TRP_Translation_Render{
 
                         // convert to a node
                         $node_from_value = TranslatePress\str_get_html(html_entity_decode(htmlspecialchars_decode($attr_value, ENT_QUOTES)), true, true, TRP_DEFAULT_TARGET_CHARSET, false, TRP_DEFAULT_BR_TEXT, TRP_DEFAULT_SPAN_TEXT);
+	                    if ( $node_from_value === false ){
+		                    continue;
+	                    }
                         foreach ($node_from_value->find('trp-gettext') as $nfv_row) {
                             $nfv_row->outertext = $nfv_row->innertext();
                             $row->setAttribute($attr_name, $node_from_value->save() );
@@ -479,6 +487,9 @@ class TRP_Translation_Render{
 	    }
 
         $html = TranslatePress\str_get_html($trpremoved, true, true, TRP_DEFAULT_TARGET_CHARSET, false, TRP_DEFAULT_BR_TEXT, TRP_DEFAULT_SPAN_TEXT);
+	    if ( $html === false ){
+		    return $trpremoved;
+	    }
 
         $no_translate_selectors = apply_filters( 'trp_no_translate_selectors', array( '#wpadminbar' ), $TRP_LANGUAGE );
 
@@ -669,6 +680,9 @@ class TRP_Translation_Render{
         // We need to save here in order to access the translated links too.
 	    $html = $html->save();
         $html = TranslatePress\str_get_html($html, true, true, TRP_DEFAULT_TARGET_CHARSET, false, TRP_DEFAULT_BR_TEXT, TRP_DEFAULT_SPAN_TEXT);
+	    if ( $html === false ){
+		    return $html;
+	    }
 
         // force custom links to have the correct language
         foreach( $html->find('a[href!="#"]') as $a_href)  {
