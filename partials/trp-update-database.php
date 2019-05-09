de78d130e558a6c03013aa33047daf50b62a3471
Ricardo Moraleida <ricardo.moraleida@get10up.com>
2019-05-09 17:12:29 -0300

    security fixes trp-update-database

--- partials/trp-update-database.php
+++ partials/trp-update-database.php
@@ -1,10 +1,10 @@
 <div id="trp-addons-page" class="wrap">
 
-    <h1> <?php _e( 'TranslatePress Database Updater', 'translatepress-multilingual' );?></h1>
+    <h1> <?php esc_html_e( 'TranslatePress Database Updater', 'translatepress-multilingual' );?></h1>
 
     <div class="grid feat-header">
         <div class="grid-cell">
-            <h2><?php _e('Updating TranslatePress tables', 'translatepress-multilingual' );?> </h2>
+            <h2><?php esc_html_e('Updating TranslatePress tables', 'translatepress-multilingual' );?> </h2>
 	        <div id="trp-update-database-progress">
 
             </div>
