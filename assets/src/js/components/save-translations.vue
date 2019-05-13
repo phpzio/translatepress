76ed1d47e2723ce21ac2744eea0b163738cda659
Razvan Mocanu <razvan@cozmoslabs.com>
2019-05-13 12:06:10 +0300

    Add support for saving slug translation

--- assets/src/js/components/save-translations.vue
+++ assets/src/js/components/save-translations.vue
@@ -35,6 +35,7 @@
                 else {
                     this.saveStringType( 'gettext' )
                     this.saveStringType( 'regular' )
+                    this.saveStringType( 'postslug' )
                 }
             },
             saveStringType( typeSlug ){
