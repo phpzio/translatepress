4d0f6ee2fc060329a437b42d801991fc8fdb225d
MadalinUngureanu <ungureanu.madalin@gmail.com>
2019-07-09 11:47:53 +0300

    internationalized the account page

--- partials/addons-settings-page.php
+++ partials/addons-settings-page.php
@@ -5,8 +5,8 @@
 
     <div class="grid feat-header">
         <div class="grid-cell">
-            <h2>Advanced Addons</h2>
-            <p>These addons extend your translation plugin and are available in the Developer, Business and Personal plans.</p>
+            <h2><?php _e( 'Advanced Addons', 'translatepress-multilingual' );?></h2>
+            <p><?php _e( 'These addons extend your translation plugin and are available in the Developer, Business and Personal plans.', 'translatepress-multilingual' );?></p>
         </div>
     </div>
 
@@ -14,7 +14,7 @@
         <div class="grid-cell" style="overflow:hidden;">
             <a href="https://translatepress.com/pricing/" target="_blank"><img src="<?php echo esc_url( plugins_url('../assets/images/seo_icon_translatepress.png', __FILE__) ) ?>" alt="SEO" style="float: left; margin: 0 1.5rem 1.5rem 0;"></a>
             <h3><a href=" <?php echo trp_add_affiliate_id_to_link('https://translatepress.com/pricing/?utm_source=wpbackend&utm_medium=clientsite&utm_content=addons_tab&utm_campaign=tpfree') ?> " target="_blank"> SEO Pack</a></h3>
-            <p>SEO support for page slug, page title, description and facebook and twitter social graph information. </br> The HTML lang attribute is properly set.</p>
+            <p><?php _e( 'SEO support for page slug, page title, description and facebook and twitter social graph information. </br> The HTML lang attribute is properly set.', 'translatepress-multilingual' );?></p>
         </div>
     </div>
 
@@ -22,15 +22,15 @@
         <div class="grid-cell" style="overflow:hidden;">
             <a href="https://translatepress.com/pricing/" target="_blank"><img src="<?php echo esc_url( plugins_url('../assets/images/multiple_lang_icon.png', __FILE__) ) ?>" alt="Multiple Languages" style="float: left; margin: 0 1.5rem 1.5rem 0;"></a>
             <h3><a href=" <?php echo trp_add_affiliate_id_to_link('https://translatepress.com/pricing/?utm_source=wpbackend&utm_medium=clientsite&utm_content=addons_tab&utm_campaign=tpfree') ?> " target="_blank"> Multiple Languages</a></h3>
-            <p>Add as many languages as you need for your project to go global.<br>
-                Publish your language only when all your translations are done. </p>
+            <p><?php _e( 'Add as many languages as you need for your project to go global.<br>
+                Publish your language only when all your translations are done.', 'translatepress-multilingual' );?> </p>
         </div>
     </div>
 
     <div class="grid feat-header">
         <div class="grid-cell">
-            <h2>Pro Addons</h2>
-            <p>These addons extend your translation plugin and are available in the Business and Developer plans.</p>
+            <h2><?php _e( 'Pro Addons', 'translatepress-multilingual' );?></h2>
+            <p><?php _e( 'These addons extend your translation plugin and are available in the Business and Developer plans.', 'translatepress-multilingual' );?></p>
         </div>
     </div>
 
@@ -38,7 +38,7 @@
         <div class="grid-cell" style="overflow:hidden;">
             <a href="https://translatepress.com/pricing/" target="_blank"><img src="<?php echo esc_url( plugins_url('../assets/images/auto-detect-language-add-on.png', __FILE__) ) ?>" alt="Automatic User Language Detection" style="float: left; margin: 0 1.5rem 1.5rem 0;"></a>
             <h3><a href=" <?php echo trp_add_affiliate_id_to_link('https://translatepress.com/pricing/?utm_source=wpbackend&utm_medium=clientsite&utm_content=addons_tab&utm_campaign=tpfree') ?> " target="_blank"> Automatic User Language Detection</a></h3>
-            <p>Automatically redirects new visitors to their preferred language based on browser settings or IP address</br> and remembers the last visited language.</p>
+            <p><?php _e( 'Automatically redirects new visitors to their preferred language based on browser settings or IP address</br> and remembers the last visited language.', 'translatepress-multilingual' );?></p>
         </div>
     </div>
 
@@ -46,7 +46,7 @@
         <div class="grid-cell" style="overflow:hidden;">
             <a href="https://translatepress.com/pricing/" target="_blank"><img src="<?php echo esc_url( plugins_url('../assets/images/translator-accounts-addon.png', __FILE__) ) ?>" alt="Translator Account" style="float: left; margin: 0 1.5rem 1.5rem 0;"></a>
             <h3><a href=" <?php echo trp_add_affiliate_id_to_link('https://translatepress.com/pricing/?utm_source=wpbackend&utm_medium=clientsite&utm_content=addons_tab&utm_campaign=tpfree') ?> " target="_blank"> Translator Accounts</a></h3>
-            <p>Create translator accounts for new users or allow existing users <br/>that are not administrators to translate your website.</p>
+            <p><?php _e( 'Create translator accounts for new users or allow existing users <br/>that are not administrators to translate your website.', 'translatepress-multilingual' );?></p>
         </div>
     </div>
 
@@ -54,7 +54,7 @@
         <div class="grid-cell" style="overflow:hidden;">
             <a href="https://translatepress.com/pricing/" target="_blank"><img src="<?php echo esc_url( plugins_url('../assets/images/view-as-addon.png', __FILE__) ) ?>" alt="Browse As User Role" style="float: left; margin: 0 1.5rem 1.5rem 0;"></a>
             <h3><a href=" <?php echo trp_add_affiliate_id_to_link('https://translatepress.com/pricing/?utm_source=wpbackend&utm_medium=clientsite&utm_content=addons_tab&utm_campaign=tpfree') ?> " target="_blank"> Browse As User Role</a></h3>
-            <p>Navigate your website just like a particular user role would. <br/>Really useful for dynamic content or hidden content that appears for particular users.</p>
+            <p><?php _e( 'Navigate your website just like a particular user role would. <br/>Really useful for dynamic content or hidden content that appears for particular users.', 'translatepress-multilingual' );?></p>
         </div>
     </div>
 
@@ -62,14 +62,14 @@
         <div class="grid-cell" style="overflow:hidden;">
             <a href="https://translatepress.com/pricing/" target="_blank"><img src="<?php echo esc_url( plugins_url('../assets/images/menu_based_on_lang.png', __FILE__) ) ?>" alt="Navigation Based on Language" style="float: left; margin: 0 1.5rem 1.5rem 0;"></a>
             <h3><a href=" <?php echo trp_add_affiliate_id_to_link('https://translatepress.com/pricing/?utm_source=wpbackend&utm_medium=clientsite&utm_content=addons_tab&utm_campaign=tpfree') ?> " target="_blank"> Navigation Based on Language</a></h3>
-            <p>Configure different menu items for different languages.</p>
+            <p><?php _e( 'Configure different menu items for different languages.', 'translatepress-multilingual' );?></p>
         </div>
     </div>
 
     <div class="grid feat-header">
         <div class="grid-cell">
-            <h2>Free Addons</h2>
-            <p>Extend your translation plugin with these free addons.</p>
+            <h2><?php _e( 'Free Addons', 'translatepress-multilingual' );?></h2>
+            <p><?php _e( 'Extend your translation plugin with these free addons.', 'translatepress-multilingual' );?></p>
         </div>
     </div>
 
@@ -77,7 +77,7 @@
         <div class="grid-cell" style="overflow:hidden;">
             <a href="https://translatepress.com/docs/addons/language-get-parameter/" target="_blank"><img src="<?php echo esc_url( plugins_url('../assets/images/get_param_addon.jpg', __FILE__) ) ?>" alt="Language by GET parameter" style="float: left; margin: 0 1.5rem 1.5rem 0;"></a>
             <h3 id="language-by-get-parameter"><a href=" <?php echo trp_add_affiliate_id_to_link('https://translatepress.com/pricing/?utm_source=wpbackend&utm_medium=clientsite&utm_content=addons_tab&utm_campaign=tpfree') ?> " target="_blank"> Language by GET parameter </a></h3>
-            <p>Use GET parameter to encode language in the url, replacing the language directory. </br> Your urls will look like this: www.example.com?lang=en</p>
+            <p><?php _e( 'Use GET parameter to encode language in the url, replacing the language directory. </br> Your urls will look like this: www.example.com?lang=en', 'translatepress-multilingual' );?></p>
         </div>
     </div>
 </div>
