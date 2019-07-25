469cd75f9d112b7fab99802047734cdf09d5ae4f
Razvan Mocanu <razvan@cozmoslabs.com>
2019-07-25 15:48:21 +0300

    Added custom code for auto translating slug

---
+++ includes/advanced-settings/enable-auto-translate-slug.php
@@ -0,0 +1,22 @@
+<?php
+
+add_filter( 'trp_register_advanced_settings', 'trp_register_enable_auto_translate_slug', 30 );
+function trp_register_enable_auto_translate_slug( $settings_array ){
+	$settings_array[] = array(
+		'name'          => 'enable_auto_translate_slug',
+		'type'          => 'checkbox',
+		'label'         => esc_html__( 'Automatically translate slugs', 'translatepress-multilingual' ),
+		'description'   => wp_kses( __( 'Generate automatic translations of slugs for posts, pages and Custom Post Types.<br/>Requires <a href="https://translatepress.com/docs/addons/seo-pack/" title="TranslatePress Add-on SEO Pack documentation" target="_blank"> SEO Pack Add-on</a> to be installed and activated.', 'translatepress-multilingual' ), array( 'br' => array(), 'a' => array( 'href' => array(), 'title' => array(), 'target' => array() ) ) ),
+	);
+	return $settings_array;
+}
+
+add_filter('trp_machine_translate_slug', 'trp_enable_auto_translate_slug');
+function trp_enable_auto_translate_slug($allow) {
+
+	$option = get_option( 'trp_advanced_settings', true );
+	if ( isset( $option['enable_auto_translate_slug'] ) && $option['enable_auto_translate_slug'] === 'yes' ) {
+		return true;
+	}
+	return $allow;
+}
--- includes/advanced-settings/skip-dynamic-selectors.php
+++ includes/advanced-settings/skip-dynamic-selectors.php
@@ -9,7 +9,7 @@ function trp_register_skip_dynamic_selectors( $settings_array ){
 			'selector' => __('Selector', 'translatepress-multilingual' ),
 		),
 		'label'         => esc_html__( 'Exclude from dynamic translation', 'translatepress-multilingual' ),
-		'description'   => wp_kses( __( 'Do not dynamically translate strings that are found in html nodes matching these selectors.<br>Excludes all the children of a HTML node matching these selectors from being translated using JavaScript.<br/>These strings will still be translated on the server side if possible.', 'translatepress-multilingual' ), array( 'br' => array() ) ),
+		'description'   => wp_kses( __( 'Do not dynamically translate strings that are found in html nodes matching these selectors.<br>Excludes all the children of HTML nodes matching these selectors from being translated using JavaScript.<br/>These strings will still be translated on the server side if possible.', 'translatepress-multilingual' ), array( 'br' => array() ) ),
 	);
 	return $settings_array;
 }
