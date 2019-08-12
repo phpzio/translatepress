99234b1edf992edc90dc38923a557715fd54cb24
Georgian Cocora <vjstink@gmail.com>
2019-08-12 15:26:49 +0300

    Add a comment to explain how it works.

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ let mix = require('laravel-mix');
 mix.js( 'assets/src/js/trp-editor.js', 'assets/js' )
     .sass( 'assets/src/scss/trp-editor.scss', 'assets/css/' )
     .browserSync( {
+        //create a .env file in the project root with the variable from below: MIX_LOCAL_URL=pms.test
         proxy : process.env.MIX_LOCAL_URL,
         files : [
             '**/*.php',
