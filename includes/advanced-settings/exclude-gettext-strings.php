31d5fd1bb98dd691f12d1be2357c0b050fe2be99
Georgian Cocora <vjstink@gmail.com>
2019-09-02 15:29:24 +0300

    [6293] Load Exclude From Gettext filter only when necessary.

--- includes/advanced-settings/exclude-gettext-strings.php
+++ includes/advanced-settings/exclude-gettext-strings.php
@@ -10,7 +10,7 @@ function trp_register_exclude_gettext_strings( $settings_array ){
 								'domain' => __('Domain', 'translatepress-multilingual')
 							),
 		'label'         => esc_html__( 'Exclude Gettext Strings', 'translatepress-multilingual' ),
-		'description'   => wp_kses( __( 'Exclude these strings from being translated as Gettext strings. Leave the domain empty to take into account any gettext string.<br/>Can still be translated as regular strings.', 'translatepress-multilingual' ), array( 'br' => array() ) ),
+		'description'   => wp_kses( __( 'Exclude these strings from being translated as Gettext strings. <br/>Can still be translated as regular strings.', 'translatepress-multilingual' ), array( 'br' => array() ) ),
 	);
 	return $settings_array;
 }
@@ -18,8 +18,30 @@ function trp_register_exclude_gettext_strings( $settings_array ){
 /**
  * Exclude gettext from being translated
  */
-add_filter('gettext', 'trpc_exclude_strings', 1000, 3 );
-function trpc_exclude_strings ($translation, $text, $domain ){
+add_action( 'init', 'trp_load_exclude_strings' );
+function trp_load_exclude_strings(){
+	$is_ajax_on_frontend = TRP_Translation_Manager::is_ajax_on_frontend();
+
+	/* on ajax hooks from frontend that have the init hook ( we found WooCommerce has it ) apply it earlier */
+	if( $is_ajax_on_frontend ){
+		add_action( 'wp_loaded', 'trp_load_exclude_strings_gettext' );
+	}
+	else{//otherwise start from the wp_head hook
+		add_action( 'wp_head', 'trp_load_exclude_strings_gettext', 100 );
+	}
+
+	//if we have woocommerce installed and it is not an ajax request add a gettext hook starting from wp_loaded and remove it on wp_head
+	if( class_exists( 'WooCommerce' ) && !$is_ajax_on_frontend ){
+		// WooCommerce launches some ajax calls before wp_head, so we need to apply_gettext_filter earlier to catch them
+		add_action( 'wp_loaded', 'trp_load_exclude_strings_gettext', 19 );
+	}
+}
+
+function trp_load_exclude_strings_gettext() {
+	add_filter('gettext', 'trp_exclude_strings', 1000, 3 );
+}
+
+function trp_exclude_strings ($translation, $text, $domain ){
 	$option = get_option( 'trp_advanced_settings', true );
 	if ( isset( $option['exclude_gettext_strings'] ) ) {
 
@@ -27,10 +49,6 @@ function trpc_exclude_strings ($translation, $text, $domain ){
 			if ( $domain === $value && $text === $option['exclude_gettext_strings']['string'][$key] ){
 				return $text;
 			}
-
-            if ( $domain === '' && $text === $option['exclude_gettext_strings']['string'][$key] ){
-                return $text;
-            }
 		}
 	}
 	return $translation;
